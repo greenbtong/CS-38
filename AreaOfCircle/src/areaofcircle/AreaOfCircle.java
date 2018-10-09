60780114a405c26696dd0a9a5f2105c4823c3905
Brian T <thetongbrian@gmail.com>
2018-10-08 20:32:41 -0700

    add circle

---
+++ AreaOfCircle/src/areaofcircle/AreaOfCircle.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areaofcircle;
+
+/**
+ *
+ * @author btong3
+ */
+public class AreaOfCircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        double radius;
+        double area;
+        radius = 3.5;
+        area = PI * radius * radius; 
+        System.out.println ( "the area of the circle is " + area );
+        
+        // TODO code application logic here
+    }
+}
